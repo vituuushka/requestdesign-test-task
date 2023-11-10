5c975fa8c768b38d728fc334a80975e948894190
vituuushka <usmanova.vita13@gmail.com>
2023-11-11 00:38:23 +0400

    feat: menu

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 document.querySelector('.burger-button').addEventListener('click', function() {
 this.classList.toggle('active');
 document.querySelector('.burger-items').classList.toggle('open')
-
+document.querySelector('.shadow').classList.toggle('open')
+document.querySelector('body').classList.toggle('no-scroll')
 })
+
+document.querySelector('.account').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.account-items').classList.toggle('open')
+    })
