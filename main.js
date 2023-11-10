5d86af510c172db7639831029049f8249a830eb1
vituuushka <usmanova.vita13@gmail.com>
2023-11-10 23:19:00 +0400

    dfghd

---
+++ main.js
@@ -0,0 +1,5 @@
+document.querySelector('.burger-button').addEventListener('click', function() {
+this.classList.toggle('active');
+document.querySelector('.burger-items').classList.toggle('open')
+
+})
